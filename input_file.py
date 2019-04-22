d98d3de5d70288d20dc3d09b1f65adc248d62eac
kelaw <48778128+kelaw@users.noreply.github.com>
2019-04-22 04:46:16 +0100

    Add files via upload

---
+++ input_file.py
@@ -0,0 +1,59 @@
+def input_choice():
+    choice_input = input('Please enter Choice: ')
+    return choice_input
+
+
+def input_value0():
+    # input order no
+    value = int(input('enter order no: '))
+
+    return value
+
+
+def input_value1():
+    # input content received
+    value = int(input('enter content received: '))
+
+    return value
+
+
+def input_value2():
+    # input rate content
+    value = int(input('enter rate content: '))
+
+    return value
+
+
+def input_value3():
+    # input empty received
+    value = int(input('enter empty received: '))
+
+    return value
+
+
+def input_value4():
+    # input rate empty
+    value = int(input('enter Rate Empty: '))
+
+    return value
+
+
+def input_value5():
+    # input empty Returned
+    value = int(input('enter Empty Returned: '))
+
+    return value
+
+
+def input_value6():
+    # input Wrong Bottle
+    value = int(input('enter Wrong Bottle: '))
+
+    return value
+
+
+def input_value7():
+    # input Cash Pay
+    value = int(input('enter Cash Pay: '))
+
+    return value
